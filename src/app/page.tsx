69cd80135fdbffa97ddc1e475acf8a358d1f8cd8
hkdas5464 <hkdas5464@gmail.com>
2024-04-22 16:09:54 +0530

    added card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection";
+import Wychooseus from "@/components/ui/Wychooseus";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       < HeroSection/>
+      <FeaturedSection/>
+      <Wychooseus/>
     </main>
   );
 }
