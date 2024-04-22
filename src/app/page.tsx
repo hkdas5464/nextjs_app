c38c88afba0a38896060f03a0d91ab16bcdc2b6f
hkdas5464 <hkdas5464@gmail.com>
2024-04-22 16:45:40 +0530

    adder hover card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinar from "@/components/UpcomingWebinar";
 import Wychooseus from "@/components/ui/Wychooseus";
 import Image from "next/image";
 
@@ -9,6 +11,8 @@ export default function Home() {
       < HeroSection/>
       <FeaturedSection/>
       <Wychooseus/>
+      <TestimonialCards/>
+      <UpcomingWebinar/>
     </main>
   );
 }
